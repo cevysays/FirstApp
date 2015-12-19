62e88c68fdd91a9d47f71da6f433f5b2ba497fd9
Cevy Yufindra <cevy.yufindra@mail.ugm.ac.id>
2015-12-19 23:39:37 +0700

    Initial Commit

---
+++ FirstApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstApp
+//
+//  Created by Cevy Yufindra on 12/19/15.
+//  Copyright © 2015 Cevy Yufindra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstApp
+//
+//  Created by Cevy Yufindra on 12/19/15.
+//  Copyright © 2015 Cevy Yufindra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
